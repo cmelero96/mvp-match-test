e14b2e9fa9b9ee73c9a0ebf95942d383f5f12d48
Carlos Melero <cmelero96@gmail.com>
2022-05-12 19:48:47 +0200

    Removed HelloWorld component and updated App name

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,12 @@
       </q-toolbar>
     </q-header>
 
-    <q-page-container>
-      <HelloWorld />
-    </q-page-container>
+    <q-page-container> </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "LayoutDefault",
-
-  components: {
-    HelloWorld,
-  },
+  name: "App",
 };
 </script>
